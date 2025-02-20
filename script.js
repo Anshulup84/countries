820dbf2f3b4badff4dd784fcab3ccca9e034ce56
Anshul <avenger720720@gmail.com>
2025-02-20 18:26:14 +0530

    card functionality implemented

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-// fetch('https://restcountries.com/v3.1/all').then((res) => res.json())
-// .then((data) => {
-//     data.forEach(country => {
-//         console.log(country);
+const countriesContainer = document.querySelector(".countries-container");
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((country) => {
+        console.log(country);
+        
+      const countryCard = document.createElement("a");
+      countryCard.classList.add("countrycard");
 
-//     });
+      countryCard.innerHTML = `<a href="#" class="country-card">
+                        <img src="${country.flags.svg}" alt="flag">
+                        <div class="card-text">
+                            <h3 class="card-title">${country.name.common}</h3>
+                            <p><b>Population: </b>${country.population.toLocaleString('en-IN')}</p>
+                            <p><b>Region: </b>${country.region}</p>
+                            <p><b>Capital: </b>${country.capital}</p>
+                        </div>
+                       
+                    </a>`;
 
-// })
-
-const countriesContainer = document.querySelector('.countries-container')
-
-const countryCard = document.createElement("a");
-countryCard.classList.add("countrycard");
-
-
-
-const cardHTML = `<a href="#" class="country-card">
-                <img src="https://flagcdn.com/gs.svg" alt="flag">
-                <div class="card-text">
-                    <h3 class="card-title">britain</h3>
-                    <p><b>Population: </b>81,770,900</p>
-                    <p><b>Region: </b>Europe</p>
-                    <p><b>Capital: </b>Berlin</p>
-                </div>
-               
-            </a>`;
-
-countryCard.innerHTML = cardHTML;
-
-countriesContainer.append(countryCard)
+      countriesContainer.append(countryCard);
+    });
+  });
